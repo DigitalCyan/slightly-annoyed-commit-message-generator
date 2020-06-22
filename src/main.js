f3e72712a4f0041a01e3958ebd77708a187931b9
DigitalCyan <marinoljubetic8@gmail.com>
2020-06-22 03:41:11 +0200

    fixed some paths

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const express = require("express");
 const fetcher = require("./fetcher");
 const app = express();
 
 const port = 3000 | process.env.PORT;
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/api", (req, res) => {
     res.send(fetcher.getMessage())
