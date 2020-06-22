aa9a9a273df562e3fb971a011aaa04d3fd94b0b4
DigitalCyan <marinoljubetic8@gmail.com>
2020-06-22 03:43:49 +0200

    Fixed the port

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fetcher = require("./fetcher");
 const app = express();
 
-const port = 3000 | process.env.PORT;
+const port = process.env.PORT | 3000;
 
 app.use(express.static(path.join(__dirname, "../public")));
 
