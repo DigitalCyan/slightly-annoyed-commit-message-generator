294939d79e75e997daef34cfde96af811defc6f8
DigitalCyan <marinoljubetic8@gmail.com>
2020-06-29 18:16:41 +0000

    added a few rootkits

--- src/main.js
+++ src/main.js
@@ -3,11 +3,12 @@ const express = require("express");
 const fetcher = require("./fetcher");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/api", (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.send(fetcher.getMessage())
 })
 
